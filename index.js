ae9b0f9bb5004d915fda53326d2819c6e63c0ca4
kxshiii <brian.gikaru@student.moringaschool.com>
2025-03-15 20:03:22 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat() {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield", "Ralph");
+  };
+  function destructivelyPrependCat(){
+    cats.length=0;
+    cats.unshift("Bob", "Milo", "Otis", "Garfield")
+  };
+  function destructivelyRemoveLastCat(){   
+    cats.pop();
+  };
+  function destructivelyRemoveFirstCat(){
+    cats.shift();
+  }
+  function appendCat(name) {
+    return [...cats, name];
+  };
+  function prependCat(name){
+    return [name, ...cats]
+  };
+  function removeLastCat(){
+    return ['Milo', 'Otis']
+  }
+  function removeFirstCat(){
+    return ['Otis', 'Garfield']
+  }
